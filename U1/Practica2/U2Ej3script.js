d9e8e003c295ab2b4c307fffda1692e2122c31a9
Nerea <nerearodriguez24@gmail.com>
2017-09-22 16:42:40 +0200

    Cambio

--- U1/Practica2/U2Ej3script.js
+++ U1/Practica2/U2Ej3script.js
@@ -1,1 +1,3 @@
-console.info("Las rosas son rojas. \n \t El mar es azul. \n \t \t No sé rimar.");
+
+var texto = ("Las rosas son rojas. \n \t El mar es azul. \n \t \t No sé rimar.");
+console.log(texto);
