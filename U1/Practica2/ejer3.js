23e47f8d018fbecb2f4b20359085ab145a247ada
Nerea <nerearodriguez24@gmail.com>
2017-09-22 15:30:26 +0200

    Cambio

--- U1/Practica2/ejer3.js
+++ U1/Practica2/ejer3.js
@@ -1,3 +1,3 @@
-console.info("Esto es la info");
+console.info("JavaScript es muy \'divertido\' y muy \'facil\' de aprender.");
 document.write("Esto es un documento.write");
 console.warn("Esto es un aviso");
