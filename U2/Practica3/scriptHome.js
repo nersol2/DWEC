05920af829e9f093fc406b56abb116261eb662ea
Nerea <nerearodriguez24@gmail.com>
2017-10-27 18:32:16 +0200

    scr

--- U2/Practica3/scriptHome.js
+++ U2/Practica3/scriptHome.js
@@ -1,2 +1,17 @@
-var s = new Surtido(productos);
-s.dibujarSurtido();
+var p = new Surtido(productos);
+
+// // var micompra = [productos[0], productos[3]];
+// s.dibujarSurtido(micompra);
+p.dibujarSurtido();
+
+function recarga(){
+
+}
+function verPorPrecioMas(p){
+  p.ordenPorPrecioMas();
+  p.dibujarSurtido();
+}
+function verPorPrecioMenos(p){
+  p.ordenPorPrecioMenos();
+  p.dibujarSurtido();
+}
