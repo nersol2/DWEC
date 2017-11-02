9b8f1bc308881a3b6dda27464516e59a4f715319
Nerea <nerearodriguez24@gmail.com>
2017-11-02 19:42:10 +0100

    scripthome

--- U2/Practica3/scriptHome.js
+++ U2/Practica3/scriptHome.js
@@ -20,8 +20,12 @@ function verVegano(){
 }
 
 function navegarCarrito(){
-  window.location="carrito.html";
+  window.open("carrito.html");
 }
 function condCompraOnline(){
   //Mostrar en otra ventana emergente las condiciones de compra y un boton que permita cerrarlas.
+  var a = window.open("", "_black","width=400, height=400");
+  a.document.write("<h1>Condiciones de las compras</h1>");
+  a.document.write("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>")
+  a.document.write("<br><button onclick='window.close()'>Cerrar ventana ")
 }
